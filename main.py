697482721ee8f50443cbc203d6a368265175093e
Marion <mrobert148@gmail.com>
2023-03-24 12:34:42 +0100

    render about and contact pages

--- main.py
+++ main.py
@@ -5,8 +5,18 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+def home():
     return render_template("index.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
 if __name__ == '__main__':
