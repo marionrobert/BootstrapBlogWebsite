03ea0f038edab7b7d79a1c27954ae5a9e2d12f3a
Marion <mrobert148@gmail.com>
2023-03-24 12:59:43 +0100

    render each blog in specific page with jinja tempalting

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 import requests
 
 response = requests.get("https://api.npoint.io/5908ca000ec67c1ee3c6")
-print(response.status_code)
+# print(response.status_code)
 all_posts = response.json()
-print(all_posts)
+# print(all_posts)
 
 app = Flask(__name__)
 
@@ -22,6 +22,15 @@ def contact():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/posts/<int:index>")
+def get_post(index):
+    returned_post = None
+    for post in all_posts:
+        if post["id"] == index:
+            returned_post = post
+    return render_template("post.html", post=returned_post)
 
 
 if __name__ == '__main__':
