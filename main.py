2a7deb1d3f989af9789c6bb35667132e0ef88a7d
Marion <mrobert148@gmail.com>
2023-03-24 12:48:38 +0100

    render all posts in homepage

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template
+import requests
 
+response = requests.get("https://api.npoint.io/5908ca000ec67c1ee3c6")
+print(response.status_code)
+all_posts = response.json()
+print(all_posts)
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts=all_posts)
 
 
 @app.route("/contact")
